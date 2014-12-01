7d7def1ea5892f68099d2ea5a0e41b16c14769b0
FranzJosephKafka <franciscco@gmail.com>
2014-12-01 20:12:38 +0100

    Más cambios de prueba

--- LogicaJoc/Class1.cs
+++ LogicaJoc/Class1.cs
@@ -5,8 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace LogicaJoc
-{
-    //fg
+{    
+    //probancdo sincrocnizacion
     public class Logica
     {
         static string psecreta;
